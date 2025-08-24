0721100f69f5d59d25cc82056ffd1917a0c9fcbf
Aleena Sultan <ags019@bucknell.edu>
2025-08-25 00:00:08 +0500

    Added prework files to repository

---
+++ Aleena_Sultan_CodePath_Prework/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Aleena_Sultan_CodePath_Prework
+//
+//  Created by Aleena Sultan on 24/08/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+        
+    }
+    
+    func changeColor() -> UIColor{
+        
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
